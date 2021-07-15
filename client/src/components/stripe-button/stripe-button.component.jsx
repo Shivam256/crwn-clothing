b4e5d0d07a531a94b9ff9df35346ac8653ed2cca
Shivam256 <shivamgavandi256@gmail.com>
2021-07-15 14:57:17 +0530

    seperating app into client folder, added express server to handle backend stripe process

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({price}) => {
   const priceForStripe = price*100;
   const publishableKey = 'pk_test_51HBB8kD1wiynfcDEjtAnrGvPSiIWnCiRGtPQCMMyPtv0Y7CxHq6n9DDF276JX60EHhmpCHHvVZWamw1YOmvs6QSt000PCtPQae';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Succesful!!');
-  }
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+    .then(response => {
+      alert('succesful payment');
+    })
+    .catch(error => {
+      console.log('Payment Error: ', error);
+      alert(
+        'There was an issue with your payment! Please make sure you use the provided credit card.'
+      );
+    });
+  };
 
   return(
     <StripeCheckout
@@ -16,7 +32,7 @@ const StripeCheckoutButton = ({price}) => {
       name='CRWN Clothing Ltd.'
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image='https://image.shutterstock.com/image-vector/crown-icon-logo-template-260nw-1177149952.jpg'
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel='Pay Now'
